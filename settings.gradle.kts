dc9e5b359efec7e28c7e851d67e385d1ac28377e
Sebastian Wyrazik <swyrazik@gmail.com>
2022-08-06 16:33:40 +0300

    Introduced dependency catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 rootProject.name = "monster-sanctuary-companion"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("junit", "5.9.0")
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+        }
+    }
+}
