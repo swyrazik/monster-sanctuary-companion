dac951c9ef149efb87cdafd12a1706ff454a13d1
Sebastian Wyrazik <swyrazik@gmail.com>
2022-08-07 14:24:04 +0300

    Testing of a failed test

--- testing/end-to-end/tests/src/test/java/com/swyrazik/monstersanctuary/companion/testing/endtoend/tests/DockerBasedTest.java
+++ testing/end-to-end/tests/src/test/java/com/swyrazik/monstersanctuary/companion/testing/endtoend/tests/DockerBasedTest.java
@@ -1,11 +1,12 @@
 package com.swyrazik.monstersanctuary.companion.testing.endtoend.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DockerBasedTest {
     
     @Test
     void test() {
-        
+        Assertions.fail(new RuntimeException("WHOOPS"));
     }
 }
