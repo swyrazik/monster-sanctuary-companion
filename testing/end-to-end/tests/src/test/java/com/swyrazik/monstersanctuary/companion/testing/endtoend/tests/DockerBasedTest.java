c9ede3e87ac962619103200ea3f70789b8564218
Sebastian Wyrazik <swyrazik@gmail.com>
2022-08-07 13:28:42 +0300

    Added testing:end-to-end:tests project, which includes an empty test.

---
+++ testing/end-to-end/tests/src/test/java/com/swyrazik/monstersanctuary/companion/testing/endtoend/tests/DockerBasedTest.java
@@ -0,0 +1,11 @@
+package com.swyrazik.monstersanctuary.companion.testing.endtoend.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class DockerBasedTest {
+    
+    @Test
+    void test() {
+        
+    }
+}
